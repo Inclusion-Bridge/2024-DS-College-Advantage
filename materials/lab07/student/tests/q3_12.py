372afc063f6e8a840e2b55b8df86f007ef660587
Ifenaike Alexander <alexander.ifenaike@gmail.com>
2024-02-02 13:46:49 +0100

    Update Labs and tests

--- materials/lab07/student/tests/q3_12.py
+++ materials/lab07/student/tests/q3_12.py
@@ -1,5 +1,7 @@
 OK_FORMAT = True
-test = {'name': 'q3_12', 'points': 1, 'suites': [{'cases': [{'code': '>>> empirical_P > 0.05\nTrue', 'hidden': False, 'locked': False,
-                                                             "failure_message":"""The value of `empirical_P` is incorrect! Expected value to exceed 0.05!""",
-                                                            "success_message":"""All answers provided are correct!"""}], 
+test = {'name': 'q3_12', 'points': 1, 'suites': [{'cases': [{'code': '>>> (empirical_P > 0.05) & (empirical_P < 0.3)\nTrue', 'hidden': False, 'locked': False,
+                                                            "failure_message":"""The value of `empirical_P` is incorrect! Expected value is a value between 0.05 and 0.3!"""},
+                                                            {'code': '>>> empirical_P == 0.26\nTrue', 'hidden': False, 'locked': False,
+                                                                "failure_message":"""The value of `empirical_P` is incorrect! Check again!""",
+                                                                "success_message":"""All answers provided are correct!"""}], 
                                                             'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
