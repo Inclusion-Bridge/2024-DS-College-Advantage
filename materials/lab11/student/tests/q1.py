f4cc6ea9cb23e6611b16eff42b8f51ac9d5758bb
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2024-05-05 10:00:10 +0100

    Delete materials/lab11 directory

--- materials/lab11/student/tests/q1.py
+++
@@ -1,9 +0,0 @@
-OK_FORMAT = True
-test = {   'name': 'q1',
-            'points': 1,
-            'suites': [ {'cases': [ {'code': '>>> len(arr)==5\nTrue', 'hidden': False, 'locked': False,
-                                    "failure_message":"""Incorrect: Expected length of `arr` to be five. Try again!"""},
-                                    {'code': '>>> list(arr)==[1,2,3,4,5]\nTrue', 'hidden': False, 'locked': False,
-                                    "failure_message":"""Incorrect: some of the elements in the `arr` array are incorrect. Double check with the requiremnets in question 1!""",
-                                    "success_message":"""All answers provided are correct!"""}], 
-                                    'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- materials/lab11/student/tests/q2a.py
+++
@@ -1,7 +0,0 @@
-OK_FORMAT = True
-test = {   'name': 'q1',
-            'points': 1,
-            'suites': [ {'cases': [ {'code': '>>> summation(3)==78\nTrue', 'hidden': False, 'locked': False,
-                                    "failure_message":"""Failed test case: summation(3). The function `summation()` is wrongly defined.""",
-                                    "success_message":"""All answers provided are correct!"""}], 
-                                    'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
